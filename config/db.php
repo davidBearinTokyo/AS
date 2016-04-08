1b261e4cbce94a566fa7e2a0c5cfb40d302ed654
daiwei.wang <daiwei.wang@rakuten.com>
2016-04-08 19:50:04 +0900

    MASTER: repo created

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost; dbname=yii2basic',
+    'username' => 'davidBear',
+    'password' => '861020David',
+    'charset' => 'utf8',
+];
